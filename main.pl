5edc5d6f90a6fb3a23b4349e0147515be3ac9536
osynligare <nic.nicholas1907@outlook.com>
2017-11-11 15:20:32 +0700

    apa

--- main.pl
+++ main.pl
@@ -1,1 +1,3 @@
-start:-write('Welcome to the 77th Hunger Games!\nYou have been chosen as one of the lucky conestants. Be the last man standing and you will be rembered as \none of the victor\n').
+start :-
+    write('Welcome to the 77th Hunger Games!\nYou have been chosen as one of the lucky conestants. Be the last man standing and you will be rembered as \none of the victor\n').
+    
