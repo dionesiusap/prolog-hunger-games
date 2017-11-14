a9b6d52053ed39a26647eeba696b80d91180a538
Ayrton <ayrton.niwarlangga@gmail.com>
2017-11-14 21:59:23 +0700

    player bisa move pake dynamic fact

--- main.pl
+++ main.pl
@@ -1,4 +1,4 @@
-setup :- consult(enemy), consult(weapon), consult(item).
+setup :- consult(enemy), consult(weapon), consult(item),consult('player.pl'),initPlayer.
 start:-setup,write('Welcome to the 77th Hunger Games!\nYou have been chosen as one of the lucky contestants. Be the last man standing and you will be rembered as \none of the victors\n\n\n'),writeEnemy,help.
 help:-write('Available commands:\nstart.   --start the games!\nhelp.   --show available commands\nquit.   --quit the game\nlook.   --look around you\nn.s.e.w.   --move\nmap.   --look at the map and detect enemies(need radar to use)\ntake(Object).   --pick up an object\ndrop(Object).   --drop an object\nuse(Object).   --use an object\nattack.   --attack enemy that crosses your path\nstatus.   --show your status\nsave(Filename).   --save your game\nload(Filename).   --load previously saved game\n').
 pemain([100,100,100,'none','Food','Apple']).
