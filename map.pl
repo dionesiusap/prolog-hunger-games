76b71b07810872d2bb106d76a0a3dc22842ce948
Ayrton <ayrton.niwarlangga@gmail.com>
2017-11-18 23:42:30 +0700

    takes

--- map.pl
+++ map.pl
@@ -12,15 +12,15 @@ writeCol(I,22):-I1 is I+1,write('\n'),writeRow(I1,0).
 writeCol(I,J):-elmt(J,I),J1 is J+1,writeCol(I,J1).
 
 elmt(I,J):-tembok(I,J),write('#').
-elmt(I,J):-e1(I,J,'alive',_),write('E'),!.
-elmt(I,J):-e2(I,J,'alive',_),write('E'),!.
-elmt(I,J):-e3(I,J,'alive',_),write('E'),!.
-elmt(I,J):-e4(I,J,'alive',_),write('E'),!.
-elmt(I,J):-e5(I,J,'alive',_),write('E'),!.
-elmt(I,J):-e6(I,J,'alive',_),write('E'),!.
-elmt(I,J):-e7(I,J,'alive',_),write('E'),!.
-elmt(I,J):-e8(I,J,'alive',_),write('E'),!.
-elmt(I,J):-e9(I,J,'alive',_),write('E'),!.
-elmt(I,J):-e10(I,J,'alive',_),write('E'),!.
+elmt(I,J):-e1(I,J,'alive',_,_),write('E'),!.
+elmt(I,J):-e2(I,J,'alive',_,_),write('E'),!.
+elmt(I,J):-e3(I,J,'alive',_,_),write('E'),!.
+elmt(I,J):-e4(I,J,'alive',_,_),write('E'),!.
+elmt(I,J):-e5(I,J,'alive',_,_),write('E'),!.
+elmt(I,J):-e6(I,J,'alive',_,_),write('E'),!.
+elmt(I,J):-e7(I,J,'alive',_,_),write('E'),!.
+elmt(I,J):-e8(I,J,'alive',_,_),write('E'),!.
+elmt(I,J):-e9(I,J,'alive',_,_),write('E'),!.
+elmt(I,J):-e10(I,J,'alive',_,_),write('E'),!.
 elmt(I,J):-player(I,J,'alive',_,_,_,_,_,_),write('P'),!.
 elmt(I,J):-write(' ').
--- player.pl
+++ player.pl
@@ -5,10 +5,10 @@
 :-dynamic(player/9).
 
 initPlayer:-random(1,21,X),random(1,11,Y),asserta(player(X,Y,'alive',100,100,100,1,'','')).
-n:-player(X,Y,S,Lp,H,T,W,I1,I2),Y\==1,Y1 is Y - 1,retract(player(X,Y,S,Lp,H,T,W,I1,I2)),asserta(player(X,Y1,S,Lp,H,T,W,I1,I2)).
-e:-player(X,Y,S,Lp,H,T,W,I1,I2),X\==20,X1 is X + 1,retract(player(X,Y,S,Lp,H,T,W,I1,I2)),asserta(player(X1,Y,S,Lp,H,T,W,I1,I2)).
-s:-player(X,Y,S,Lp,H,T,W,I1,I2),Y\==10,Y1 is Y + 1,retract(player(X,Y,S,Lp,H,T,W,I1,I2)),asserta(player(X,Y1,S,Lp,H,T,W,I1,I2)).
-w:-player(X,Y,S,Lp,H,T,W,I1,I2),X\==1,X1 is X - 1,retract(player(X,Y,S,Lp,H,T,W,I1,I2)),asserta(player(X1,Y,S,Lp,H,T,W,I1,I2)).
+n:-player(X,Y,S,Lp,H,T,W,I1,I2),Y\==1,Y1 is Y - 1,retract(player(X,Y,S,Lp,H,T,W,I1,I2)),asserta(player(X,Y1,S,Lp,H,T,W,I1,I2)),moveenemy.
+e:-player(X,Y,S,Lp,H,T,W,I1,I2),X\==20,X1 is X + 1,retract(player(X,Y,S,Lp,H,T,W,I1,I2)),asserta(player(X1,Y,S,Lp,H,T,W,I1,I2)),moveenemy.
+s:-player(X,Y,S,Lp,H,T,W,I1,I2),Y\==10,Y1 is Y + 1,retract(player(X,Y,S,Lp,H,T,W,I1,I2)),asserta(player(X,Y1,S,Lp,H,T,W,I1,I2)),moveenemy.
+w:-player(X,Y,S,Lp,H,T,W,I1,I2),X\==1,X1 is X - 1,retract(player(X,Y,S,Lp,H,T,W,I1,I2)),asserta(player(X1,Y,S,Lp,H,T,W,I1,I2)),moveenemy.
 
 look:-player(X,Y,S,Lp,H,T,W,I1,I2),writeItem(X,Y).
 
