2b67abdcbfeed3f00915d5b2e38614ed6092f1e3
osynligare <nic.nicholas1907@outlook.com>
2017-11-20 00:38:56 +0700

    pul

--- Attack.pl
+++ Attack.pl
@@ -1,39 +1,63 @@
+:-dynamic(itemE1/4).
+:-dynamic(itemE2/4).
+:-dynamic(itemE3/4).
+:-dynamic(itemE4/4).
+:-dynamic(itemE5/4).
+:-dynamic(itemE6/4).
+:-dynamic(itemE7/4).
+:-dynamic(itemE8/4).
+:-dynamic(itemE9/4).
+:-dynamic(itemE10/4).
 /*command untuk attack, hanya bisa menyerang musuh yang ada di petak yang sama) */
 attack :- player(X,Y,S,Lp,H,T,W,L), W =:= 1, e1(X,Y,'alive',Atk1,O), retract(e1(X,Y,'alive',Atk1,O)), 
 		asserta(e1(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
-		F is H-Atk1, \+ isDead(F), asserta(player(X,Y,S,Lp,F,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\nk.'), moveenemy.
+		F is Lp-Atk1, \+ isDead(F), asserta(player(X,Y,S,F,H,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\nk.'), asserta(itemE1(X,Y,'available',O)),write('Your enemy drop '),write(O),moveenemy,write('\n').
 attack :- player(X,Y,S,Lp,H,T,W,L), W =:= 1, e2(X,Y,'alive',Atk1,O), retract(e2(X,Y,'alive',Atk1,O)), 
-		asserta(e1(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
-		F is H-Atk1, \+ isDead(F), asserta(player(X,Y,S,Lp,F,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), moveenemy.
+		asserta(e2(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
+		F is Lp-Atk1, \+ isDead(F), asserta(player(X,Y,S,F,H,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), asserta(itemE2(X,Y,'available',O)),write('Your enemy drop '),write(O),moveenemy,write('\n').
 attack :- player(X,Y,S,Lp,H,T,W,L), W =:= 1, e3(X,Y,'alive',Atk1,O), retract(e3(X,Y,'alive',Atk1,O)), 
-		asserta(e1(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
-		F is H-Atk1, \+ isDead(F), asserta(player(X,Y,S,Lp,F,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), moveenemy.
+		asserta(e3(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
+		F is Lp-Atk1, \+ isDead(F), asserta(player(X,Y,S,F,H,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), asserta(itemE3(X,Y,'available',O)),write('Your enemy drop '),write(O),moveenemy,write('\n').
 attack :- player(X,Y,S,Lp,H,T,W,L), W =:= 1, e4(X,Y,'alive',Atk1,O), retract(e4(X,Y,'alive',Atk1,O)), 
-		asserta(e1(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
-		F is H-Atk1, \+ isDead(F), asserta(player(X,Y,S,Lp,F,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), moveenemy.
+		asserta(e4(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
+		F is Lp-Atk1, \+ isDead(F), asserta(player(X,Y,S,F,H,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), asserta(itemE4(X,Y,'available',O)),write('Your enemy drop '),write(O),moveenemy,write('\n').
 attack :- player(X,Y,S,Lp,H,T,W,L), W =:= 1, e5(X,Y,'alive',Atk1,O), retract(e5(X,Y,'alive',Atk1,O)), 
-		asserta(e1(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
-		F is H-Atk1, \+ isDead(F), asserta(player(X,Y,S,Lp,F,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), moveenemy.
+		asserta(e5(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
+		F is Lp-Atk1, \+ isDead(F), asserta(player(X,Y,S,F,H,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), asserta(itemE5(X,Y,'available',O)),write('Your enemy drop '),write(O),moveenemy,write('\n').
 attack :- player(X,Y,S,Lp,H,T,W,L), W =:= 1, e6(X,Y,'alive',Atk1,O), retract(e6(X,Y,'alive',Atk1,O)), 
-		asserta(e1(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
-		F is H-Atk1, \+ isDead(F), asserta(player(X,Y,S,Lp,F,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), moveenemy.
+		asserta(e6(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
+		F is Lp-Atk1, \+ isDead(F), asserta(player(X,Y,S,F,H,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), asserta(itemE6(X,Y,'available',O)),write('Your enemy drop '),write(O),moveenemy,write('\n').
 attack :- player(X,Y,S,Lp,H,T,W,L), W =:= 1, e7(X,Y,'alive',Atk1,O), retract(e7(X,Y,'alive',Atk1,O)), 
-		asserta(e1(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
-		F is H-Atk1, \+ isDead(F), asserta(player(X,Y,S,Lp,F,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), moveenemy.
+		asserta(e7(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
+		F is Lp-Atk1, \+ isDead(F), asserta(player(X,Y,S,F,H,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), asserta(itemE7(X,Y,'available',O)),write('Your enemy drop '),write(O),moveenemy,write('\n').
 attack :- player(X,Y,S,Lp,H,T,W,L), W =:= 1, e8(X,Y,'alive',Atk1,O), retract(e8(X,Y,'alive',Atk1,O)), 
-		asserta(e1(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
-		F is H-Atk1, \+ isDead(F), asserta(player(X,Y,S,Lp,F,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), moveenemy.
+		asserta(e8(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
+		F is Lp-Atk1, \+ isDead(F), asserta(player(X,Y,S,F,H,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), asserta(itemE8(X,Y,'available',O)),write('Your enemy drop '),write(O),moveenemy,write('\n').
 attack :- player(X,Y,S,Lp,H,T,W,L), W =:= 1, e9(X,Y,'alive',Atk1,O), retract(e9(X,Y,'alive',Atk1,O)), 
-		asserta(e1(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
-		F is H-Atk1, \+ isDead(F), asserta(player(X,Y,S,Lp,F,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), moveenemy.
+		asserta(e9(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
+		F is Lp-Atk1, \+ isDead(F), asserta(player(X,Y,S,F,H,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), asserta(itemE9(X,Y,'available',O)),write('Your enemy drop '),write(O),moveenemy,write('\n').
 attack :- player(X,Y,S,Lp,H,T,W,L), W =:= 1, e10(X,Y,'alive',Atk1,O), retract(e10(X,Y,'alive',Atk1,O)), 
-		asserta(e1(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
-		F is H-Atk1, \+ isDead(F), asserta(player(X,Y,S,Lp,F,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), moveenemy.
+		asserta(e10(X,Y,'Dead',Atk1,O)),write('an enemy killed\n'), retract(player(X,Y,S,Lp,H,T,W,L)), 
+		F is Lp-Atk1, \+ isDead(F), asserta(player(X,Y,S,F,H,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP\n'), asserta(itemE10(X,Y,'available',O)),write('Your enemy drop '),write(O),moveenemy,write('\n').
 attack :- player(_,_,_,_,_,_,W,_), W =:= 1, write('No more enemy to attack in this place').
 attack :- write('you\'re not using any weapon').
 
 isAttacked :- player(X,Y,S,Lp,H,T,W,L), e1(X,Y,'alive',Atk1,O), getattacked.
 
-getattacked :- retract(player(X,Y,S,Lp,H,T,W,L)), F is H-Atk1, \+ isDead(F), asserta(player(X,Y,S,Lp,F,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP').
+getattacked :- retract(player(X,Y,S,Lp,H,T,W,L)), F is Lp-Atk1, \+ isDead(F), asserta(player(X,Y,S,F,H,T,W,L)), write('you\'ve been wounded -'),write(Atk1), write(' HP').
 
 isDead(F) :- F =< 0, write('you\'re dead'), halt.
+
+isWin:-
+	e1(_,_,'Dead',_,_),
+	e2(_,_,'Dead',_,_),
+	e3(_,_,'Dead',_,_),
+	e4(_,_,'Dead',_,_),
+	e5(_,_,'Dead',_,_),
+	e6(_,_,'Dead',_,_),
+	e7(_,_,'Dead',_,_),
+	e8(_,_,'Dead',_,_),
+	e9(_,_,'Dead',_,_),
+	e10(_,_,'Dead',_,_),
+	e1(_,_,'Dead',_,_),
+	write('All enemy die you win').
--- player.pl
+++ player.pl
@@ -1,5 +1,5 @@
 /* baca file eksternal */
-/*(X,Y,status,Health,Hungry,Thirst,Weapon,ItemList)*/
+/*(X,Y,status,LifePoint,Hungry,Thirst,Weapon,ItemList)*/
 
 
 :-dynamic(player/8).
@@ -7,16 +7,33 @@
 initPlayer:-random(1,21,X),random(1,11,Y),look_pos(X,Y),!,initPlayer.
 initPlayer:-random(1,21,X),random(1,11,Y),asserta(player(X,Y,'alive',100,100,100,0,[])).
 
-n:-player(X,Y,S,Lp,H,T,W,L),Y\==1,Y1 is Y - 1,look_pos(X,Y1),!,write('cant move there\n'),moveenemy.
-n:-player(X,Y,S,Lp,H,T,W,L),Y\==1,Y1 is Y - 1,retract(player(X,Y,S,Lp,H,T,W,L)),asserta(player(X,Y1,S,Lp,H,T,W,L)),lookTerrain(X,Y1),moveenemy,!.
-e:-player(X,Y,S,Lp,H,T,W,L),X\==20,X1 is X + 1,look_pos(X1,Y),!,write('cant move there\n'),moveenemy.
-e:-player(X,Y,S,Lp,H,T,W,L),X\==20,X1 is X + 1,retract(player(X,Y,S,Lp,H,T,W,L)),asserta(player(X1,Y,S,Lp,H,T,W,L)),lookTerrain(X1,Y),moveenemy,!.
-s:-player(X,Y,S,Lp,H,T,W,L),Y\==10,Y1 is Y + 1,look_pos(X,Y1),!,write('cant move there\n'),moveenemy.
-s:-player(X,Y,S,Lp,H,T,W,L),Y\==10,Y1 is Y + 1,retract(player(X,Y,S,Lp,H,T,W,L)),asserta(player(X,Y1,S,Lp,H,T,W,L)),lookTerrain(X,Y1),moveenemy,!.
-w:-player(X,Y,S,Lp,H,T,W,L),X\==1,X1 is X - 1,look_pos(X1,Y),!,write('cant move there\n'),moveenemy.
-w:-player(X,Y,S,Lp,H,T,W,L),X\==1,X1 is X - 1,retract(player(X,Y,S,Lp,H,T,W,L)),asserta(player(X1,Y,S,Lp,H,T,W,L)),lookTerrain(X1,Y),moveenemy,!.
+n:-player(X,Y,S,Lp,H,T,W,L),\+dead,Y\==1,Y1 is Y - 1,look_pos(X,Y1),!,write('cant move there\n'),moveenemy.
+n:-player(X,Y,S,Lp,H,T,W,L),\+dead,Y\==1,Y1 is Y - 1,retract(player(X,Y,S,Lp,H,T,W,L)),H1 is H-2,T1 is T-1,asserta(player(X,Y1,S,Lp,H1,T1,W,L)),lookTerrain(X,Y1),moveenemy,!,lookNow(X,Y1).
+e:-player(X,Y,S,Lp,H,T,W,L),\+dead,X\==20,X1 is X + 1,look_pos(X1,Y),!,write('cant move there\n'),moveenemy.
+e:-player(X,Y,S,Lp,H,T,W,L),\+dead,X\==20,X1 is X + 1,retract(player(X,Y,S,Lp,H,T,W,L)),H1 is H-2,T1 is T-1,asserta(player(X1,Y,S,Lp,H1,T1,W,L)),lookTerrain(X1,Y),moveenemy,!,lookNow(X1,Y).
+s:-player(X,Y,S,Lp,H,T,W,L),\+dead,Y\==10,Y1 is Y + 1,look_pos(X,Y1),!,write('cant move there\n'),moveenemy.
+s:-player(X,Y,S,Lp,H,T,W,L),\+dead,Y\==10,Y1 is Y + 1,retract(player(X,Y,S,Lp,H,T,W,L)),H1 is H-2,T1 is T-1,asserta(player(X,Y1,S,Lp,H1,T1,W,L)),lookTerrain(X,Y1),moveenemy,!,lookNow(X,Y1).
+w:-player(X,Y,S,Lp,H,T,W,L),\+dead,X\==1,X1 is X - 1,look_pos(X1,Y),!,write('cant move there\n'),moveenemy.
+w:-player(X,Y,S,Lp,H,T,W,L),\+dead,X\==1,X1 is X - 1,retract(player(X,Y,S,Lp,H,T,W,L)),H1 is H-2,T1 is T-1,asserta(player(X1,Y,S,Lp,H1,T1,W,L)),lookTerrain(X1,Y),moveenemy,!,lookNow(X1,Y).
+
+dead:-player(_,_,_,Lp,H,T,_,_),Lp=:=0,write('You are dead'),halt.
+dead:-player(_,_,_,Lp,H,T,_,_),H=:=0,write('You are dead'),halt.
+dead:-player(_,_,_,Lp,H,T,_,_),T=:=0,write('You are dead'),halt.
 
 look:-player(X,Y,S,Lp,H,T,W,L),writeItem(X,Y).
+
+status:-player(X,Y,S,Lp,H,T,W,L),
+    write('Health: '),write(Lp),write('\n'),
+    write('Hungry: '),write(H),write('\n'),
+    write('Thirst: '),write(T),write('\n'),
+    write('Weapon: '),writeWeapon(W),write('\n'),
+    write('Inventory: '),write('\n'),writeInventory(L),write('\n').
+
+writeWeapon(1):-write('bazooka').
+writeWeapon(0):-write('bare Hands').
+
+writeInventory([]):-write('\n').
+writeInventory([H|T]):-write(H),write('\n'),writeInventory(T).
 
 writeItem(X,Y):-X1 is X-1,Y1 is Y-1,X2 is X+1,Y2 is Y+1,
     look_elmt(X1,Y1),
@@ -61,6 +78,21 @@ look_elmt(I,J):-item7(I,J,'available','food'),write('F'),!.
 look_elmt(I,J):-item8(I,J,'available','medecine'),write('M'),!.
 look_elmt(I,J):-item9(I,J,'available','medecine'),write('M'),!.
 look_elmt(I,J):-item10(I,J,'available','radar'),write('R'),!.
+look_elmt(I,J):-itemE1(I,J,'available','water'),write('W'),!.
+look_elmt(I,J):-itemE2(I,J,'available','water'),write('W'),!.
+look_elmt(I,J):-itemE3(I,J,'available','water'),write('W'),!.
+look_elmt(I,J):-itemE4(I,J,'available','food'),write('F'),!.
+look_elmt(I,J):-itemE5(I,J,'available','food'),write('F'),!.
+look_elmt(I,J):-itemE6(I,J,'available','food'),write('F'),!.
+look_elmt(I,J):-itemE7(I,J,'available','food'),write('F'),!.
+look_elmt(I,J):-itemE8(I,J,'available','medecine'),write('M'),!.
+look_elmt(I,J):-itemE9(I,J,'available','medecine'),write('M'),!.
+look_elmt(I,J):-itemE10(I,J,'available','radar'),write('R'),!.
+look_elmt(I,J):-item11(I,J,'available','bazooka'),write('B'),!.
+look_elmt(I,J):-item12(I,J,'available','bazooka'),write('B'),!.
+look_elmt(I,J):-item13(I,J,'available','bazooka'),write('B'),!.
+look_elmt(I,J):-item14(I,J,'available','bazooka'),write('B'),!.
+look_elmt(I,J):-item15(I,J,'available','bazooka'),write('B'),!.
 look_elmt(I,J):-player(I,J,'alive',_,_,_,_,_),write('P'),!.
 look_elmt(I,J):-write('-').
 
@@ -74,6 +106,46 @@ look_pos(I,J):-x13(I,J),!.
 look_pos(I,J):-x15(I,J),!.
 look_pos(I,J):-x17(I,J),!.
 look_pos(I,J):-x19(I,J),!.
+
+
+/*melihat petak dibawah ada apa*/
+lookNow(I,J):-e1(I,J,'alive',_,_),write('You see an enemy\n').
+lookNow(I,J):-e2(I,J,'alive',_,_),write('You see an enemy\n').
+lookNow(I,J):-e3(I,J,'alive',_,_),write('You see an enemy\n').
+lookNow(I,J):-e4(I,J,'alive',_,_),write('You see an enemy\n').
+lookNow(I,J):-e5(I,J,'alive',_,_),write('You see an enemy\n').
+lookNow(I,J):-e6(I,J,'alive',_,_),write('You see an enemy\n').
+lookNow(I,J):-e7(I,J,'alive',_,_),write('You see an enemy\n').
+lookNow(I,J):-e8(I,J,'alive',_,_),write('You see an enemy\n').
+lookNow(I,J):-e9(I,J,'alive',_,_),write('You see an enemy\n').
+lookNow(I,J):-e10(I,J,'alive',_,_),write('You see an enemy\n').
+lookNow(I,J):-item1(I,J,'available','water'),write('You see some water\n').
+lookNow(I,J):-item2(I,J,'available','water'),write('You see some water\n').
+lookNow(I,J):-item3(I,J,'available','water'),write('You see some water\n').
+lookNow(I,J):-item4(I,J,'available','food'),write('You see some food\n').
+lookNow(I,J):-item5(I,J,'available','food'),write('You see some food\n').
+lookNow(I,J):-item6(I,J,'available','food'),write('You see some food\n').
+lookNow(I,J):-item7(I,J,'available','food'),write('You see some food\n').
+lookNow(I,J):-item8(I,J,'available','medecine'),write('You see some medicine\n').
+lookNow(I,J):-item9(I,J,'available','medecine'),write('You see some medicine\n').
+lookNow(I,J):-item10(I,J,'available','radar'),write('You see a radar\n').
+lookNow(I,J):-itemE1(I,J,'available','water'),write('You see some water\n').
+lookNow(I,J):-itemE2(I,J,'available','water'),write('You see some water\n').
+lookNow(I,J):-itemE3(I,J,'available','water'),write('You see some water\n').
+lookNow(I,J):-itemE4(I,J,'available','food'),write('You see some food\n').
+lookNow(I,J):-itemE5(I,J,'available','food'),write('You see some food\n').
+lookNow(I,J):-itemE6(I,J,'available','food'),write('You see some food\n').
+lookNow(I,J):-itemE7(I,J,'available','food'),write('You see some food\n').
+lookNow(I,J):-itemE8(I,J,'available','medecine'),write('You see some medicine\n').
+lookNow(I,J):-itemE9(I,J,'available','medecine'),write('You see some medicine\n').
+lookNow(I,J):-itemE10(I,J,'available','radar'),write('You see a radar\n').
+lookNow(I,J):-item11(I,J,'available','bazooka'),write('you see a bazooka'),!.
+lookNow(I,J):-item12(I,J,'available','bazooka'),write('you see a bazooka'),!.
+lookNow(I,J):-item13(I,J,'available','bazooka'),write('you see a bazooka'),!.
+lookNow(I,J):-item14(I,J,'available','bazooka'),write('you see a bazooka'),!.
+lookNow(I,J):-item15(I,J,'available','bazooka'),write('you see a bazooka'),!.
+lookNow(I,J):-write(''),!.
+
 
 
 /********** TAKE, USE, AND DROP COMMANDS **********/
